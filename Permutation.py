73bbe432d23940e5f7cc7e545e490a0c0e8c3faf
LukeDude200 <croslynlc@gmail.com>
2021-12-09 18:24:37 -0400

    Made a function to pick a tower

--- Permutation.py
+++ Permutation.py
@@ -3,6 +3,33 @@ import random
 import TowerTypes as TT
 
 
+def pick_tower():
+    """
+    This function will choose a tower type and the amount of upgrades for it
+    :return: Returns the tower's type, the upgrades amount and the keybinding for the tower
+    """
+    dictionary_num = random.randint(1, 3)
+    if dictionary_num == 1:
+        tower_index = random.randint(0, len(TT.lead_towers) - 1)
+        tower_type = list(TT.lead_towers.keys())[tower_index]
+        upgrades1 = TT.lead_towers.get(tower_type)[1]()
+        upgrades2 = TT.lead_towers.get(tower_type)[2]()
+        keybind = TT.lead_towers.get(tower_type)[0]
+    elif dictionary_num == 2:
+        tower_index = random.randint(0, len(TT.camo_towers) - 1)
+        tower_type = list(TT.camo_towers.keys())[tower_index]
+        upgrades1 = TT.camo_towers.get(tower_type)[1]()
+        upgrades2 = TT.camo_towers.get(tower_type)[2]()
+        keybind = TT.camo_towers.get(tower_type)[0]
+    else:
+        tower_index = random.randint(0, len(TT.all_tower_types) - 1)
+        tower_type = list(TT.all_tower_types.keys())[tower_index]
+        upgrades1 = TT.all_tower_types.get(tower_type)[1]()
+        upgrades2 = TT.all_tower_types.get(tower_type)[2]()
+        keybind = TT.all_tower_types.get(tower_type)[0]
+    return tower_type, upgrades1, upgrades2, keybind
+
+
 class Permutation:
     def __init__(self, grid, cancel_coord):
         """
@@ -11,28 +38,9 @@ class Permutation:
         """
         self.towers_wanted = []
         self.fitness = 0
-        num_towers = random.randint(1, 10)
+        num_towers = random.randint(1, 3)
         for i in range(num_towers):
-            dictionary_num = random.randint(1, 3)
-            if dictionary_num == 1:
-                tower_index = random.randint(0, len(TT.lead_towers)-1)
-                tower_type = list(TT.lead_towers.keys())[tower_index]
-                upgrades1 = TT.lead_towers.get(tower_type)[1]()
-                upgrades2 = TT.lead_towers.get(tower_type)[2]()
-                keybind = TT.lead_towers.get(tower_type)[0]
-            elif dictionary_num == 2:
-                tower_index = random.randint(0, len(TT.camo_towers)-1)
-                tower_type = list(TT.camo_towers.keys())[tower_index]
-                upgrades1 = TT.camo_towers.get(tower_type)[1]()
-                upgrades2 = TT.camo_towers.get(tower_type)[2]()
-                keybind = TT.camo_towers.get(tower_type)[0]
-            else:
-                tower_index = random.randint(0, len(TT.all_tower_types)-1)
-                tower_type = list(TT.all_tower_types.keys())[tower_index]
-                upgrades1 = TT.all_tower_types.get(tower_type)[1]()
-                upgrades2 = TT.all_tower_types.get(tower_type)[2]()
-                keybind = TT.all_tower_types.get(tower_type)[0]
-            # Need to fix the location stuff
+            tower_type, upgrades1, upgrades2, keybind = pick_tower()
             self.towers_wanted.append(Tower(tower_type, random.choice(random.choice(grid)), upgrades1, upgrades2, keybind, cancel_coord))
 
     def place_towers(self):
